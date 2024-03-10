d76ba1ebff3322b6be39e22dd26545baddccddd3
in9 <mangogoon@gmail.com>
2024-03-10 15:57:50 +0900

    #5.13 Price Chart part Two - Chart 컴포넌트에 data Interface 추가 - apexChart 라이브러리 설치

--- 05-crypto-reacker/src/routes/Chart.tsx
+++ 05-crypto-reacker/src/routes/Chart.tsx
@@ -1,16 +1,69 @@
-// /src/routes/Chart.tsx
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcy", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcy", coinId], () =>
     fetchCoinHistory(coinId),
   );
-  return <h1>Chart!</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "price",
+              data: data?.map((price) => +price.close) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: {
+              show: false,
+            },
+            yaxis: { show: false },
+            xaxis: {
+              labels: { show: false },
+              axisTicks: { show: false },
+              axisBorder: { show: false },
+            },
+            stroke: {
+              curve: "smooth",
+              width: 5,
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
