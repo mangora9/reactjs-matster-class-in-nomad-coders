3cbe96aab777e8c91eb40652a810bb8f9efdb002
in9 <mangogoon@gmail.com>
2024-03-24 16:24:23 +0900

    #6.13 Categories - ToDo 컴포넌트 작업

--- 06-todo/src/conponents/ToDo.tsx
+++ 06-todo/src/conponents/ToDo.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import {IToDo} from "../atoms";
+import {IToDo, toDoState} from "../atoms";
+import {useSetRecoilState} from "recoil";
 
-const ToDo = ({text}: IToDo) => {
+const ToDo = ({text, category}: IToDo) => {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {currentTarget: {name}} = event;
+    console.log(`-> newCategory`, name);
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== 'TO_DO' && <button name="TO_DO" onClick={onClick}>To Do</button>}
+      {category !== 'DOING' && <button name="DOING" onClick={onClick}>Doing</button>}
+      {category !== 'DONE' && <button name="DONE" onClick={onClick}>Done</button>}
     </li>);
 };
 
