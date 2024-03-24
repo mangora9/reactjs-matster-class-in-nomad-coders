a41c44f7dfcd37c5da337370a0dc912e2ed65ecd
in9 <mangogoon@gmail.com>
2024-03-24 15:29:55 +0900

    #6.11 Add To Do - Recoil을 사용한 ToDo 아톰 생성 - Todo List 출력

--- 06-todo/src/conponents/ToDoList.tsx
+++ 06-todo/src/conponents/ToDoList.tsx
@@ -1,23 +1,39 @@
 import React from "react";
 import {useForm} from "react-hook-form";
+import {atom, useRecoilState} from "recoil";
+
+interface IToDo {
+  id: number;
+  text: string;
+  category: 'DONE' | 'DOING' | "TO_DO";
+}
 
 interface IForm {
   toDo: string;
 }
 
+const toDoState = atom<IToDo[]>({
+  key: 'toDo',
+  default: [],
+});
+
 const ToDoList = () => {
+  const [toDos, setToDos] = useRecoilState(toDoState);
   const {
     register,
     handleSubmit,
     setValue
   } = useForm<IForm>();
 
-  const handleValid = (data: IForm) => {
-    console.log(`-> toDo`, data.toDo);
+  const handleValid = ({toDo}: IForm) => {
+    setToDos((prev) => [{id: Date.now(), text: toDo, category: "TO_DO"}, ...prev]);
     setValue('toDo', '');
   };
+  console.log(toDos);
   return (
     <div>
+      <h1>To Dos</h1>
+      <hr/>
       <form onSubmit={handleSubmit(handleValid)}>
         <input
           {...register('toDo', {
@@ -27,6 +43,9 @@ const ToDoList = () => {
         />
         <button>Add</button>
       </form>
+      <ul>
+        {toDos.map((toDo) => <li key={toDo.id}>{toDo.text}</li>)}
+      </ul>
     </div>
   );
 };
