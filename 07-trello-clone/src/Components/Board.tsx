d22f7c77407379d1539e691e87c34c720ab98601
in9 <mangogoon@gmail.com>
2024-03-31 16:50:44 +0900

    #7.11 Droppable Snapshot - Board Style 작업 (snapshot 속성 이용)

--- 07-trello-clone/src/Components/Board.tsx
+++ 07-trello-clone/src/Components/Board.tsx
@@ -10,6 +10,19 @@ const Wrapper = styled.div`
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 300px;
+  display: flex;
+  flex-direction: column;
+`;
+
+interface IAreaProps {
+  isDraggingOver: boolean;
+  isDraggingFromThis: boolean;
+}
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${props => props.isDraggingOver ? 'pink' : props.isDraggingFromThis ? 'red' : 'blue'};
+  flex-grow: 1;
+  transition: background-color .3s ease-in-out;
 `;
 
 const Title = styled.h2`
@@ -29,13 +42,16 @@ const Board = ({toDos, boardId}: IBoardProps) => {
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(magic) =>
-          <div ref={magic.innerRef} {...magic.droppableProps}>
+        {(magic, snapshot) =>
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+            ref={magic.innerRef} {...magic.droppableProps}>
             {
               toDos.map((toDo, index) => <DraggableCard key={toDo} index={index} toDo={toDo}/>
               )}
             {magic.placeholder}
-          </div>
+          </Area>
         }
       </Droppable>
     </Wrapper>
