9e1385731e0b442a12babfecbfaced3139abccd3
in9 <mangogoon@gmail.com>
2024-03-31 16:33:31 +0900

    #7.10 Cross Board Movement - 다른 영역의 Board로 dnd 기능 완성하기

--- 07-trello-clone/src/App.tsx
+++ 07-trello-clone/src/App.tsx
@@ -27,28 +27,41 @@ function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = (info: DropResult) => {
     const {destination, source, draggableId} = info;
+    if (!destination) {
+      return;
+    }
 
     if (destination?.droppableId === source.droppableId) {
       // same board movement.
-      setToDos((oldToDos => {
-        const boardCopy = [...oldToDos[source.droppableId]];
+      setToDos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]];
         boardCopy.splice(source.index, 1);
         boardCopy.splice(destination?.index, 0, draggableId);
         return {
-          ...oldToDos,
+          ...allBoards,
           [source.droppableId]: boardCopy
         };
-      }));
+      });
+    } else {
+      // cross board movement.
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
+        };
+      });
     }
-
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          {
-            Object.keys(toDos).map((boardId) => <Board toDos={toDos[boardId]} boardId={boardId} key={boardId}/>)
-          }
+          {Object.keys(toDos).map((boardId) => <Board toDos={toDos[boardId]} boardId={boardId} key={boardId}/>)}
         </Boards>
       </Wrapper>
     </DragDropContext>
@@ -56,5 +69,3 @@ function App() {
 }
 
 export default App;
-
-
