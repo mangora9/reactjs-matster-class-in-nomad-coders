be20976d80fe73ae94dee116c6018d20f14430b0
in9 <mangogoon@gmail.com>
2024-05-04 17:30:29 +0900

    #9.1 Header part One - router 설정 - header 컴포넌트 설정 중.

--- 09-nomflix-clone/src/App.tsx
+++ 09-nomflix-clone/src/App.tsx
@@ -1,5 +1,26 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from "./routes/Home";
+import Tv from "./routes/Tv";
+import Search from "./routes/Search";
+import Header from "./components/Header";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header/>
+      <Switch>
+        <Route path="">
+          <Home/>
+        </Route>
+        <Route path="/tv">
+          <Tv/>
+        </Route>
+        <Route path="/search">
+          <Search/>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ 09-nomflix-clone/src/routes/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{backgroundColor: 'black', height: '200vh'}}>
+      HOME
+    </div>
+  );
+};
+
+
+export default Home;
