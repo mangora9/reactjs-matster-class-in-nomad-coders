f5101d2b87f2682c5f549def9dacc95f7a22221d
in9 <mangogoon@gmail.com>
2024-05-04 18:27:59 +0900

    #9.3 Header part Three - 현재 페이지를 표시하는 Circle 컴포넌트 애니매이션 작업 - 검색창 작업

--- 09-nomflix-clone/src/App.tsx
+++ 09-nomflix-clone/src/App.tsx
@@ -1,22 +1,22 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from "./routes/Home";
-import Tv from "./routes/Tv";
-import Search from "./routes/Search";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Header from "./components/Header";
-
+import Home from "./routes/Home";
+import Search from "./routes/Search";
+import Tv from "./routes/Tv";
+ 
 function App() {
   return (
     <Router>
       <Header/>
       <Switch>
-        <Route path="">
-          <Home/>
-        </Route>
         <Route path="/tv">
           <Tv/>
         </Route>
         <Route path="/search">
           <Search/>
+        </Route>
+        <Route path="/">
+          <Home/>
         </Route>
       </Switch>
     </Router>
