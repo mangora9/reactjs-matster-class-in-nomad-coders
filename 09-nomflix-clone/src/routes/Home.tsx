cfd7f42ff1eb07ff0aaf6d922acaab12401f1c63
in9 <mangogoon@gmail.com>
2024-05-05 15:05:11 +0900

    ## # 9.10 Box Animations part Two -슬라이더  박스 영화 정보 추가

--- 09-nomflix-clone/src/routes/Home.tsx
+++ 09-nomflix-clone/src/routes/Home.tsx
@@ -69,6 +69,20 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   }
 `;
 
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
 const rowVariants = {
   hidden: {
     x: window.outerWidth + 5,
@@ -87,10 +101,21 @@ const boxVariants = {
   },
   hover: {
     scale: 1.3,
-    y: -50,
+    y: -80,
     transition: {
       delay: 0.5,
-      duaration: 0.3,
+      duration: 0.1,
+      type: "tween",
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.1,
       type: "tween",
     },
   },
@@ -150,7 +175,11 @@ const Home = () => {
                       initial="normal"
                       variants={boxVariants}
                       transition={{type: "tween"}}
-                    />
+                    >
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
                   ))}
               </Row>
             </AnimatePresence>
