e8583deaf9b115e7f4150974a4048ff26e2a7aa2
in9 <mangogoon@gmail.com>
2024-05-05 14:55:59 +0900

    ## 9.9 Box Animations part One - 슬라이더 박스 애니매이션 추가

--- 09-nomflix-clone/src/routes/Home.tsx
+++ 09-nomflix-clone/src/routes/Home.tsx
@@ -59,6 +59,14 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   height: 200px;
   color: red;
   font-size: 66px;
+
+  &:first-child {
+    transform-origin: center left;
+  }
+
+  &:last-child {
+    transform-origin: center right;
+  }
 `;
 
 const rowVariants = {
@@ -70,6 +78,21 @@ const rowVariants = {
   },
   exit: {
     x: -window.outerWidth - 5,
+  },
+};
+
+const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: {
+      delay: 0.5,
+      duaration: 0.3,
+      type: "tween",
+    },
   },
 };
 
@@ -92,7 +115,7 @@ const Home = () => {
     }
   };
   const toggleLeaving = () => setLeaving((prev) => !prev);
-  
+
   return (
     <Wrapper>
       {isLoading ? (
@@ -123,6 +146,10 @@ const Home = () => {
                     <Box
                       key={movie.id}
                       bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                      whileHover="hover"
+                      initial="normal"
+                      variants={boxVariants}
+                      transition={{type: "tween"}}
                     />
                   ))}
               </Row>
